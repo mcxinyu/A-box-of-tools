f8451c5c900149b89861507adbcee1795c724dc2
mcxinyu <mcixnyu@foxmail.com>
2016-01-27 21:07:18 +0800

    可以读取单个文件,多个文件还有问题,sout的时候有部分乱码.

--- src/Control/C2FControl/ReadCdd.java
+++ src/Control/C2FControl/ReadCdd.java
@@ -1,9 +1,6 @@
 package Control.C2FControl;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by 跃峰 on 2016/1/27.
@@ -15,26 +12,29 @@ public class ReadCdd {
 
     public static void main(String[] args) {
         ReadCdd rc = new ReadCdd();
-        System.out.println(rc.cddContent);
+        System.out.println(rc.textFolderToString("/Users/huangyuefeng/Downloads/CDD/"));
     }
 
     public ReadCdd(){
-        cdd = new File("D:\\SZ\\变频工作\\数据采集\\CDD\\20160122\\SZ01A.Log");
-        cddContent = readToString(cdd);
+//        cdd = new File("/Users/huangyuefeng/Downloads/SZ01A.Log");
+//        cddContent = readToString(cdd);
     }
 
     /**
-     * 用于读取文本文件，返回文本内容；
+     * 用于读取单个文本文件，返回文本内容；
      * @param file
      * @return
      */
-    public static String readToString(File file) {
+    public String textToString(File file) {
         FileInputStream in = null;
         Long filelength = file.length();     //获取文件长度
-        byte[] filecontent = new byte[filelength.intValue()];
+        String fileName = file.getName();   //文件名
+        String fileType = fileName.substring(fileName.lastIndexOf("."));    //文件后缀
+
+        byte[] fileContent = new byte[filelength.intValue()];
         try {
             in = new FileInputStream(file);
-            in.read(filecontent);
+            in.read(fileContent);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,6 +45,24 @@ public class ReadCdd {
                 e.printStackTrace();
             }
         }
-        return new String(filecontent);//返回文件内容,默认编码
+        return new String(fileContent);//返回文件内容,默认编码
+    }
+
+    public String textFolderToString(String folderPath) {
+        File[] files = new File(folderPath).listFiles();
+        String connect = null;
+        if (files.length>0){
+//            for(File file:files){
+//                System.out.println(file.getName());
+//                connect += textToString(file);
+//            }
+            for (int i=0;i<files.length;i++){
+                System.out.println(files[i].getName());
+                connect += textToString(files[i]);
+            }
+        }else {
+            System.out.println("文件夹是空的");
+        }
+        return connect;
     }
 }
