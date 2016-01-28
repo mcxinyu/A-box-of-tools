fc3d2a393d3cce866c17fb985eeabcfc5be937e1
mcxinyu <mcixnyu@foxmail.com>
2016-01-28 22:49:45 +0800

    开始读取log,但是有错误. java.lang.StringIndexOutOfBoundsException: String index out of range: -3

--- src/Control/C2FControl/ReadCdd.java
+++ src/Control/C2FControl/ReadCdd.java
@@ -1,5 +1,7 @@
 package Control.C2FControl;
 
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
 import java.io.*;
 
 /**
@@ -11,58 +13,106 @@ public class ReadCdd {
     String cddContent;
 
     public static void main(String[] args) {
+        File f = new File("/Users/huangyuefeng/Downloads/CDD/SZ01A.Log");
         ReadCdd rc = new ReadCdd();
-        System.out.println(rc.textFolderToString("/Users/huangyuefeng/Downloads/CDD/"));
+        rc.processorSingleLog(f);
     }
 
     public ReadCdd(){
 //        cdd = new File("/Users/huangyuefeng/Downloads/SZ01A.Log");
-//        cddContent = readToString(cdd);
+//        cddContent = readSingleLog(cdd);
+    }
+
+    /**
+     * 读取并解析单个 cdd-log 文件
+     * @param logFile cdd-log 文件路径
+     * @return
+     */
+    private void processorSingleLog(File logFile){
+        int len = 0;
+        String line = "";
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+        StringBuffer str = new StringBuffer("");
+
+        try {
+            fis = new FileInputStream(logFile);
+            isr= new InputStreamReader(fis);
+            br = new BufferedReader(isr);
+
+            while((line=br.readLine())!=null){
+//                if(len != 0){   // 处理换行符的问题
+//                    str.append("\r\n"+line);
+//                }
+//                else{
+//                    str.append(line);
+//                }
+
+
+                if (line.substring(3)=="***"){
+                    len++;
+                    System.out.println(len);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                br.close();
+                isr.close();
+                fis.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
     }
 
     /**
      * 用于读取单个文本文件，返回文本内容；
-     * @param file
+     * @param logFile cdd-log 路径地址
      * @return
      */
-    public String textToString(File file) {
-        FileInputStream in = null;
-        Long filelength = file.length();     //获取文件长度
-        String fileName = file.getName();   //文件名
-        String fileType = fileName.substring(fileName.lastIndexOf("."));    //文件后缀
-
+    public String readSingleLog(File logFile) {
+        FileInputStream fis = null;
+        Long filelength = logFile.length();    //获取文件长度
+        String fileName = logFile.getName();   //文件名
+//    String fileType = fileName.substring(fileName.lastIndexOf("."));    //文件后缀
         byte[] fileContent = new byte[filelength.intValue()];
         try {
-            in = new FileInputStream(file);
-            in.read(fileContent);
+            fis = new FileInputStream(logFile);
+            fis.read(fileContent);
 
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
             try {
-                in.close();
+                fis.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        return new String(fileContent);//返回文件内容,默认编码
+        return new String(fileContent); //返回文件内容,默认编码
     }
 
     public String textFolderToString(String folderPath) {
         File[] files = new File(folderPath).listFiles();
-        String connect = null;
+        String connect ="";
         if (files.length>0){
-//            for(File file:files){
-//                System.out.println(file.getName());
-//                connect += textToString(file);
-//            }
-            for (int i=0;i<files.length;i++){
-                System.out.println(files[i].getName());
-                connect += textToString(files[i]);
+            for(File file:files){
+                System.out.println(file.getName());
+                connect += readSingleLog(file);
             }
+//            for (int i=0;i<files.length;i++){
+//                System.out.println(files[i].getName());
+//                connect += textToString(files[i]);
+//            }
         }else {
-            System.out.println("文件夹是空的");
+            System.out.println("未找到文件");
         }
+
         return connect;
     }
+
 }
