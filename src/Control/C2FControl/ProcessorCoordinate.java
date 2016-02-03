6681c2c79e9bcddb47b2a7ec4b671615334f93f7
Vicent <mcxinyu@foxmail.com>
2016-02-03 17:04:33 +0800

    完成坐标文件的读取和返回数值
    完成坐标文件的读取和返回数值
    下一步导入坐标文件、处理读取到的cdd数据并返回值、导出forte文件

--- src/Control/C2FControl/ProcessorCoordinate.java
+++ src/Control/C2FControl/ProcessorCoordinate.java
@@ -3,10 +3,22 @@ package Control.C2FControl;
 import java.io.File;
 
 /**
+ * 处理 coordinate 文件
  * Created by 跃峰 on 2016/2/2.
  */
 public class ProcessorCoordinate {
-    public void processorSingleLog(File logFile, int a){
+    public static void main(String[] args) {
+        File f = new File("D:\\SZ\\变频工作\\数据采集\\cellinfo\\coordinate.txt");
+        ProcessorCoordinate pc = new ProcessorCoordinate();
+        pc.processorSingleLog(f);
+    }
 
+    public String[] processorSingleLog(File logFile){
+        String[] coordinates = ReadFile.readSingleText(logFile).split("\\r\\n");
+        for (int i=0;i<coordinates.length;i++){
+            System.out.println(coordinates[i]);
+        }
+//        System.out.println("总共： "+(coordinates.length-1));
+        return coordinates;
     }
 }
--- src/Control/C2FControl/ReadFile.java
+++ src/Control/C2FControl/ReadFile.java
@@ -1,25 +1,21 @@
 package Control.C2FControl;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 
 /**
- * 用于读取文本文件
+ * 用于读取文件（读取文本、读取文件夹等）
  * Created by 跃峰 on 2016/2/3.
  */
-public class ReadText {
+public class ReadFile {
 
     /**
      * 读取单个 cdd-log 文件，通过“<”分列文件，并返回一个数组
      *
      * @param logFile cdd-log 文件路径
-     * @param a       随便
      * @return 返回一个数组 string [ ]
      */
-    public static String readSingleText(File logFile,int a) {
+    public static String readSingleText(File logFile) {
         FileInputStream fis = null;
         Long filelength = logFile.length();    //获取文件长度
         String fileName = logFile.getName();   //文件名
@@ -27,11 +23,16 @@ public class ReadText {
         try {
             fis = new FileInputStream(logFile);
             fis.read(fileContent);
+        }catch (FileNotFoundException e) {
+//            e.printStackTrace();
+            System.out.println("找不到指定的文件。");
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
             try {
                 fis.close();
+            }catch (NullPointerException e){
+                System.out.println("无法读取文件");
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -44,9 +45,10 @@ public class ReadText {
      * 读取单个 cdd-log 文件，返回一个 HashMap<Integer,String>
      *
      * @param logFile cdd-log 文件路径
+     * @param a       随便
      * @return 返回一个 HashMap<Integer,String>
      */
-    public static HashMap readSingleText(File logFile){
+    public static HashMap readSingleText(File logFile,int a){
         int len = 0;
         String line = "";
         FileInputStream fis = null;
@@ -78,4 +80,27 @@ public class ReadText {
         return logContent;
     }
 
+    /**
+     * 读取文件夹
+     *
+     * @param folderPath the folder path
+     * @return string string
+     */
+    public String textFolderToString(String folderPath) {
+        File[] files = new File(folderPath).listFiles();
+        String connect ="";
+        if (files.length>0){
+            for(File file:files){
+                System.out.println(file.getName());
+                connect += ReadFile.readSingleText(file);
+            }
+//            for (int i=0;i<files.length;i++){
+//                System.out.println(files[i].getName());
+//                connect += textToString(files[i]);
+//            }
+        }else {
+            System.out.println("未找到文件");
+        }
+        return connect;
+    }
 }
