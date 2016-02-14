b69b937e99e7d9f472a349c1cf08ce4422b65eb3
Vicent <mcxinyu@foxmail.com>
2016-02-14 13:47:04 +0800

    完成坐标文件中的参数提取
    准备导入坐标文件、处理读取到的cdd数据并返回值、导出forte文件

--- src/Control/C2FControl/ProcessorCoordinate.java
+++ src/Control/C2FControl/ProcessorCoordinate.java
@@ -8,17 +8,36 @@ import java.io.File;
  */
 public class ProcessorCoordinate {
     public static void main(String[] args) {
+        long startTime=System.currentTimeMillis();
+
         File f = new File("D:\\SZ\\变频工作\\数据采集\\cellinfo\\coordinate.txt");
         ProcessorCoordinate pc = new ProcessorCoordinate();
         pc.processorSingleLog(f);
+
+        long endTime=System.currentTimeMillis();
+        System.out.println("程序运行时间： "+(endTime-startTime)+"ms");
     }
 
-    public String[] processorSingleLog(File logFile){
+    public String[][] processorSingleLog(File logFile){
         String[] coordinates = ReadFile.readSingleText(logFile).split("\\r\\n");
-        for (int i=0;i<coordinates.length;i++){
-            System.out.println(coordinates[i]);
+        String[] temp = new String[11];
+        // cellCoordinate : Sector + Longitude + Latitude + LAC + CI + Azimuth;
+        String[][] cellCoordinate = new String[coordinates.length][6];
+
+        for (int i=0;i<coordinates.length;i++) {
+            temp = coordinates[i].split("\\t");
+            cellCoordinate[i][0] = temp[0];
+            cellCoordinate[i][1] = temp[1];
+            cellCoordinate[i][2] = temp[10];
+            cellCoordinate[i][3] = temp[2];
+            cellCoordinate[i][4] = temp[3];
+            cellCoordinate[i][5] = temp[4];
+//            for (int j = 0; j < cellCoordinate[i].length; j++) {
+//                System.out.print(cellCoordinate[i][j] + " ");
+//            }
+//            System.out.println();
         }
 //        System.out.println("总共： "+(coordinates.length-1));
-        return coordinates;
+        return cellCoordinate;
     }
 }
