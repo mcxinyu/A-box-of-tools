77810bc6533dd298d8f0939d55b4d336f7981d1c
mcxinyu <mcxinyu@foxmail.com>
2016-04-13 22:33:05 +0800

    完善更新C2I
    完成更新c2i的逻辑

--- src/Control/UpdateC2IControl/Update.java
+++ src/Control/UpdateC2IControl/Update.java
@@ -2,7 +2,10 @@ package Control.UpdateC2IControl;
 
 import Control.CommonControl.ReadFile;
 
+import javax.swing.*;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by 跃峰 on 2016/4/13.
@@ -10,8 +13,10 @@ import java.io.File;
 public class Update {
     public static void main(String[] args) {
         Update uc = new Update();
-        //uc.readC2I(new File("D:\\SZ\\变频工作\\数据采集\\model\\c2i.txt"));
-        uc.readNewDate(new File("D:\\SZ\\变频工作\\数据采集\\model\\new.txt"));
+        File obj = new  File("D:\\SZ\\变频工作\\数据采集\\model\\RxLevels.txt");
+        String newData[][] = uc.readNewDate(new File("D:\\SZ\\变频工作\\数据采集\\model\\newdata.txt"));
+        String exportPath = "D:\\SZ\\变频工作\\数据采集\\model\\";
+        uc.replace(obj,newData,exportPath);
     }
     public String readC2I(File c2i){
         String content = null;
@@ -20,6 +25,7 @@ public class Update {
         }
         return content;
     }
+
     public String[][] readNewDate(File newDataFile){
         String newData[][] = null;
         if (newDataFile != null && newDataFile.isFile()){
@@ -31,16 +37,27 @@ public class Update {
                 newData[i][0] = temp[0];
                 newData[i][1] = temp[1];
             }
-
-
-            //for (int i = 0; i < newData.length; i++) {
-            //    System.out.println(newData[i][0]+"-"+newData[i][1]);
-            //}
-
-            //for (int i = 0; i < line.length; i++) {
-            //    System.out.println(line[i]);
-            //}
         }
         return newData;
+    }
+
+    public void replace(File c2i,String[][] newData,String exportPath){
+
+        String content = null;
+        if (c2i != null && c2i.isFile()) {
+            content = ReadFile.readSingleText(c2i);
+        }
+
+        File exportFile = new File(exportPath + "\\c-new.txt");
+
+        try (FileWriter exportFileFW = new FileWriter(exportFile)) {
+            for (int i = 0; i < newData.length; i++) {
+                content = content.replace(newData[i][0],newData[i][1]);
+            }
+            exportFileFW.write(content);
+        }catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null,"执行失败，找不到文件或文件正在被使用。");
+        }
     }
 }
