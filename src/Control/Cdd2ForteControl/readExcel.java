a1f95bafffd8300aa268e1d0d1a327fba9a3b328
Vicent <mcxinyu@foxmail.com>
2016-06-01 16:26:46 +0800

    准备开始读取网优之家的数据生成forte文件

---
+++ src/Control/Cdd2ForteControl/readExcel.java
@@ -0,0 +1,14 @@
+package Control.Cdd2ForteControl;
+
+/**
+ * Created by 跃峰 on 2016/6/1.
+ */
+public class ReadExcel {
+    public static void main(String[] args) {
+        new ReadExcel();
+    }
+
+    public ReadExcel(){
+
+    }
+}
